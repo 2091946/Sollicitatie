6213518db7f4d68805a73972fc0d280b7ea3ef34
Rocco <rocco1999@hotmail.nl>
2019-10-14 09:38:55 +0200

    First Commit
    Basic Loginsystem and a basic index page with some links.

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<?php 
+
+// Includes navbar for easy use
+include 'partials/menu.php';
+
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+	header("location: login/login.php");
+	exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="login/main.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+    <div class="page-header">
+		<h1>Hi, <b style="color:green;"><?php echo htmlspecialchars($_SESSION["username"]); ?>.<br></b> WIP Sollicitatie Systeem</h1>
+	</div>
+
+	<div class="page-info">
+		<h4>Plan of action:</h4><br>
+        <ul>
+            <li>Create a loginsystem. ( DONE )</li>
+            <li>Create a my profile page ( X )</li>
+            <li>Create a system to upload files ( X )</li>
+            <li>Create a system to upload job offers ( X )</li>
+            <li>Style the pages ( X )</li>
+        </ul>
+	</div>
+
+	<div class="footer">
+		<a href="login/reset-password.php" class="btn btn-warning">Reset Your Password</a>
+		<a href="login/logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+	</div>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ public/partials/menu.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Sollicitatie Systeem</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="php\distudios\loginsystem\index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="joboffers.php">Job Offers</a>
+      </li>
+      <li class="nav-item">
+      	<a class="nav-link" href="pages/parallax.php">My Profile</a>
+      </li>
+        </div>
+      </li>
+  </div>
+</nav>
